7e258ef7da30790639fbbc2041ac4436e61f8234
Марат Керимов <mchammer0550@gmail.com>
2026-03-19 18:41:51 +0300

    1.0.0

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var ellipse = sender as Ellipse;
+            var transform = ellipse.RenderTransform as RotateTransform;
+            if (transform != null)
+            { }
+            transform.Angle += 45;
+        }
+    }
+}
